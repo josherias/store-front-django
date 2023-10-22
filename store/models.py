d40e1d5e6a337a2529c9cc217e450e257cce305f
Josh Erias <josherias10@gmail.com>
2023-10-22 16:43:55 +0300

    Added database

--- store/models.py
+++ store/models.py
@@ -9,9 +9,11 @@ class Promotion(models.Model):
 
 class Collection(models.Model):
     title = models.CharField(max_length=255)
+    featured_product = models.ForeignKey('Product', on_delete=models.SET_NULL, null=True, related_name='+')
 
 class Product(models.Model):
     title = models.CharField(max_length=255)
+    slug = models.SlugField(null=TRUE)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
     inventory = models.IntegerField()
