5a8f001255144d975065f04730ae1a85eb7bdffe
Josh Erias <josherias10@gmail.com>
2023-10-23 09:28:37 +0300

    Change on Prdocut model

--- store/models.py
+++ store/models.py
@@ -15,7 +15,7 @@ class Product(models.Model):
     title = models.CharField(max_length=255)
     slug = models.SlugField(null=TRUE)
     description = models.TextField()
-    price = models.DecimalField(max_digits=6, decimal_places=2)
+    unit_price = models.DecimalField(max_digits=6, decimal_places=2)
     inventory = models.IntegerField()
     updated_at = models.DateTimeField(auto_now=True)
     collection = models.ForeignKey(Collection, on_delete=models.PROTECT)
